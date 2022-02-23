2b0b8de3cdb992254274d006406810a1c8cf9efd
Suraj49 <1507surajkumar@gmail.com>
2022-02-23 13:50:57 +0530

    commit

---
+++ reverseArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"Enter Size of array"<<endl;
+    cin>>size;
+    int A[size];
+    cout<<"Enter the elements"<<endl;
+    for(int i=0;i<size;i++){
+        cin>>A[i];
+    }
+    for(int i=1;i<=size;i++){
+        cout<<A[size-i];
+    }
+    return 0;
+}
